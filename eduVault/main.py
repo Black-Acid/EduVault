aaedc5406015c8d9e5175aaef6c577a992d8e32c
Prince Adams <princeadams193@gmail.com>
2026-08-01 13:20:13 +0000

    added cors

--- eduVault/main.py
+++ eduVault/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from eduVault.database import check_db_connection, get_db
@@ -14,6 +15,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://pastquestionsandtutorials.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
